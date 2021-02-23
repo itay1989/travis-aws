f7fe1ddb6874188d8e5b311c84ee1a41d9f65633
itay1989 <63843840+itay1989@users.noreply.github.com>
2021-02-23 11:36:47 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi There , this is the windows test. and good news it works
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
