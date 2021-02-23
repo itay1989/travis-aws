0b3faf84ae30bbf246274a5699f5286f5f240ae1
itay1989 <63843840+itay1989@users.noreply.github.com>
2021-02-23 19:10:40 +0200

    fixxed some code in the app and app test js files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There , this is the windows test. and good news it works
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
